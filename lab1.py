4e6810ff01694756b00e1094772e30bfa3d70ca2
huma1994 <maximus.innase@gmail.com>
2022-10-24 16:14:09 +0545

    “v2”

--- lab1.py
+++ lab1.py
@@ -1,6 +1,9 @@
 def application(environ, start_response):
     status = '200 OK'
     output = b'CI CD pipeline using jenkin apache python and developer GIT'
+
+    
+    output = b'boom boom'
     response_headers = [('Content-type', 'text/plain'),
     ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
